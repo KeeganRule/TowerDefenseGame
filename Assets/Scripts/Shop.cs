65ab40c38e563c1acafadd7bbb15aacff583c08b
KeeganRule <37841537+KeeganRule@users.noreply.github.com>
2018-11-27 14:48:40 -0500

    Map/ waypoint changes

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    public TurretBlueprint standardTurret;
+    public TurretBlueprint missleLauncher;
+    public Text money;
+
+    BuildManager buildManager;
+
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+    public void SelectStandardTurret()
+    {
+        buildManager.SelectTurretToBuild(standardTurret);
+    }
+
+    public void SelectMissleLauncher()
+    {
+        buildManager.SelectTurretToBuild(missleLauncher);
+    }
+
+}
